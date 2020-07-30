7b0f2aaeb04976c5f07d42dd42b615b7390c8655
karlrom <ksavrom@gmail.com>
2020-07-30 17:23:15 +0800

    basic-modals

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,66 @@
 import React from 'react';
-import {Button , Modal } from 'react-bootstrap';
+import {Container, Row, Col, Button , Modal } from 'react-bootstrap';
 class App extends React.Component {
   constructor(){
     super()
     this.state={
-      show:false
+      showModal1:false,
+      showModal2:false
     }
   }
-  handleModal()
+
+  handleClose(id) 
   {
-    this.setState({show:!this.state.show})
+    this.setState({showModal1: false});
   }
+  handleModal(id)
+  {
+    this.setState({showModal1: true})
+  }
+ 
   render() {
     return (
     <div>
       {/*<h1> Bootstrap Modal in React</h1>*/}
-      <Button onClick={()=>{this.handleModal()}}>Open Modal</Button>
-      <Modal show ={this.state.show} onHide={()=>this.handleModal()} >
-        <Modal.Header >Modal Head Part</Modal.Header>
-        <Modal.Body>
-          Hi , React modal is here
+      <Button onClick={()=>{this.handleModal()}}>Package Selection</Button>
+      <Modal show ={this.state.showModal1} >
+        <Modal.Header >Package Selection</Modal.Header>
+        <Modal.Body className = "show-grid">
+          <Container>
+            <Row>
+              <Col>
+                <b>The package you chose</b>
+              </Col>
+              <col>
+              arrow
+              </col>
+              <Col>
+                <b>Select a deal</b>
+              </Col>
+            </Row>
+          </Container>
         </Modal.Body>
         <Modal.Footer>
-          <Button  onClick={()=>{this.handleModal()}}>
-            Close Modal
+          <Button class = "text-left" onClick={()=>{this.handleClose()}}>
+            Change Package
+          </Button>
+          <Button  onClick={() => this.setState({ showModal2:true, showModal1:false})}>
+            Proceed
           </Button>
         </Modal.Footer>
       </Modal>
+
+      <Modal show ={this.state.showModal2} >
+        <Modal.Header >Summary of Deals</Modal.Header>
+        <Modal.Body>
+          SOD
+        </Modal.Body>
+        <Modal.Footer>
+          <Button  onClick={()=>this.setState({ showModal2:false, showModal1:false})}>
+            Close Modal
+          </Button>
+        </Modal.Footer>
+      </Modal>      
     </div>
     );
   }
